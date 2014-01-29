670ec8bae3d181370c2b7f41dd18f9d6ec0e29a3
Todd Olsen <todd.olsen@me.com>
2014-01-29 12:33:45 -0500

    Initial Commit

---
+++ TKOKit Classes/TKOFlippedClipView.h
@@ -0,0 +1,13 @@
+//
+//  TKOFlippedClipView.h
+//  InfoBarStackView
+//
+//  Created by Todd Olsen on 1/26/14.
+//  Copyright (c) 2014 Apple Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TKOFlippedClipView : NSClipView
+
+@end
---
+++ TKOKit Classes/TKOFlippedClipView.m
@@ -0,0 +1,18 @@
+//
+//  TKOFlippedClipView.m
+//  InfoBarStackView
+//
+//  Created by Todd Olsen on 1/26/14.
+//  Copyright (c) 2014 Apple Inc. All rights reserved.
+//
+
+#import "TKOFlippedClipView.h"
+
+@implementation TKOFlippedClipView
+
+- (BOOL)isFlipped
+{
+    return YES;
+}
+
+@end
