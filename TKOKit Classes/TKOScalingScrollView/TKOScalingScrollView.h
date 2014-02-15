3c205e5e43f15e8593a535568b1c7be38f525ef8
Todd Olsen <todd.olsen@me.com>
2014-02-15 14:54:16 -0500

    Clean up

--- TKOKit Classes/TKOScalingScrollView/TKOScalingScrollView.h
+++ TKOKit Classes/TKOScalingScrollView/TKOScalingScrollView.h
@@ -1,19 +1,12 @@
-
-/*
-     File: TKOScalingScrollView.h
- {
-     Abstract: NSScrollView subclass to support scaling content.
-      Version: 1.9
-     Copyright (C) 2013 Apple Inc. All Rights Reserved.
- }
- 
- Originally from Apple's TextEdit app. Modified by TKO.
- 
- */
+//
+//  TKOScalingScrollView.h
+//  TKOKit
+//
+//  Created by Todd Olsen on 2/15/14.
+//  Copyright (c) 2014 Todd Olsen. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
-
-@class NSPopUpButton;
 
 @interface TKOScalingScrollView : NSScrollView
 
@@ -21,16 +14,7 @@
 @property (nonatomic) CGFloat documentWidth;
 @property (nonatomic) CGFloat documentInset;
 
-//- (void)setScaleFactor:(CGFloat)factor
-//           adjustPopup:(BOOL)flag;
-
-//- (CGFloat)scaleFactor;
-
 - (void)frameDidChange:(NSNotification *)notification;
-
-//- (IBAction)zoomToActualSize:(id)sender;
-//- (IBAction)zoomIn:(id)sender;
-//- (IBAction)zoomOut:(id)sender;
 
 @end
 
--- TKOKit Classes/TKOScalingScrollView/TKOScalingScrollView.m
+++ TKOKit Classes/TKOScalingScrollView/TKOScalingScrollView.m
@@ -1,28 +1,23 @@
-
-/*
-     File: TKOScalingScrollView.m
- {
-     Abstract: NSScrollView subclass to support scaling content.
-     Version: 1.9
-     Copyright (C) 2013 Apple Inc. All Rights Reserved.
- }
- 
- Originally from Apple's TextEdit app. Modified by TKO.
- 
- */
+//
+//  TKOScalingScrollView.m
+//  TKOKit
+//
+//  Created by Todd Olsen on 2/15/14.
+//  Copyright (c) 2014 Todd Olsen. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
 #import "TKOScalingScrollView.h"
 
 @implementation TKOScalingScrollView
 
-//- (void)awakeFromNib
-//{
-//    [super awakeFromNib];
-//    [self setAllowsMagnification:YES];
-//    [self setMaxMagnification:16.0];
-//    [self setMinMagnification:0.25];
-//}
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self setAllowsMagnification:YES];
+    [self setMaxMagnification:16.0];
+    [self setMinMagnification:0.25];
+}
 
 - (void)setFitToWidth:(BOOL)fitToWidth
 {
@@ -46,29 +41,6 @@
     }
 }
 
-//- (CGFloat)scaleFactor
-//{
-//    return [self magnification];
-//}
-//
-//- (void)setScaleFactor:(CGFloat)newScaleFactor
-//{
-//    [self setMagnification:newScaleFactor];
-//}
-//
-//- (void)setScaleFactor:(CGFloat)newScaleFactor
-//           adjustPopup:(BOOL)flag
-//{
-//    [self setScaleFactor:newScaleFactor];
-//}
-
-    /* Action methods */
-
-//- (IBAction)zoomToActualSize:(id)sender
-//{
-//    [[self animator] setMagnification:1.0];
-//}
-
 - (void)frameDidChange:(NSNotification *)notification
 {
     if (self != [notification object])
@@ -82,16 +54,10 @@
         NSLog(@"zero width");
         return;
     }
-    
-//    NSTextView * tv = self.documentView;
-//    NSLog(@"tv class: %@", NSStringFromClass([tv class]));
-//    
-//    NSLog(@"textviewrect 10 %@", NSStringFromRect([tv frame]));
-    
+
     NSRect visibleRect = [self documentVisibleRect];
     
     CGFloat scaleFactor = (NSWidth(visibleRect) - (1 * self.documentInset))/self.documentWidth;
-//    NSLog(@"scaleFactor %f", scaleFactor);
     NSRect scaledVisibleRect = NSMakeRect(
                                    visibleRect.origin.x,
                                    visibleRect.origin.y,
@@ -99,67 +65,17 @@
                                    visibleRect.size.height * scaleFactor
                                 );
     NSRect originalDocumentViewFrame = [self.documentView frame];
-//    NSLog(@"textviewrect 11 %@", NSStringFromRect([tv frame]));
-
-    
-//    if (NSWidth(originalDocumentViewFrame) < NSWidth(visibleRect)) {
-//        [self.documentView setFrame:NSMakeRect(
-//                                               visibleRect.origin.x,
-//                                               visibleRect.origin.y,
-//                                               visibleRect.size.width,
-//                                               newHeight
-//                                               )];
-//        NSLog(@"adjusting original rect");
-//        return;
-//    }
-    
     [self.documentView scaleUnitSquareToSize:NSMakeSize(scaleFactor, scaleFactor)];
-//    NSLog(@"textviewrect 12 %@", NSStringFromRect([tv frame]));
-
     [self.documentView setFrame:NSMakeRect(
                                            originalDocumentViewFrame.origin.x,
                                            originalDocumentViewFrame.origin.y,
                                            originalDocumentViewFrame.size.width * scaleFactor,
                                            originalDocumentViewFrame.size.height * scaleFactor
                                            )];
-//    NSLog(@"textviewrect 13 %@", NSStringFromRect([tv frame]));
-
-//    NSLog(@"\n\nvisibleRect %@, \nscaledVisibleRect %@, \noriginalDocumentViewFrame %@, \nself.documentView.frame %@\n", NSStringFromRect(visibleRect), NSStringFromRect(scaledVisibleRect), NSStringFromRect(originalDocumentViewFrame), NSStringFromRect([self.documentView frame]));
     
     CGPoint scrollPoint = NSMakePoint(scaledVisibleRect.origin.x, scaledVisibleRect.origin.y - self.documentInset * scaleFactor);
     [[self documentView] scrollPoint:scrollPoint];
 }
-
-//- (BOOL)validateMenuItem:(NSMenuItem *)menuItem
-//{
-//    switch (menuItem.tag) {
-//        case 667:
-//        case 668:
-//        case 669:
-//        case 670:
-//        case 671:
-//            return YES;
-//            break;
-//            
-//        default:
-//            return NO;
-//            break;
-//    }
-//}
-
-//- (IBAction)zoomIn:(id)sender
-//{
-//    CGFloat scaleFactor = [self scaleFactor];
-//    scaleFactor = (scaleFactor > 0.4 && scaleFactor < 0.6) ? 1.0 : scaleFactor * 2.0;
-//    [[self animator] setMagnification:scaleFactor];
-//}
-//
-//- (IBAction)zoomOut:(id)sender
-//{
-//    CGFloat scaleFactor = [self scaleFactor];
-//    scaleFactor = (scaleFactor > 1.8 && scaleFactor < 2.2) ? 1.0 : scaleFactor / 2.0;
-//    [[self animator] setMagnification:scaleFactor];
-//}
 
 /* 
     Reassure AppKit that TKOScalingScrollView supports live resize content
