2985b8bc6c0b250a58ac54b845f25633c706a5d7
Todd Olsen <todd.olsen@me.com>
2014-03-06 09:39:53 -0500

    Improvements

---
+++ TKOKit Classes/TKODisclosureView/TKODisclosingView.h
@@ -0,0 +1,19 @@
+//
+//  TKODisclosingView.h
+//  TKOProblemTemplateClassDemo
+//
+//  Created by Todd Olsen on 3/5/14.
+//  Copyright (c) 2014 Todd Olsen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TKODisclosingView : NSView
+
+@property (strong, nonatomic) NSString * title;
+@property (strong, nonatomic) NSView * contentView;
+
+- (id)initWithTitle:(NSString *)title
+        contentView:(NSView *)contentView;
+
+@end
---
+++ TKOKit Classes/TKODisclosureView/TKODisclosingView.m
@@ -0,0 +1,259 @@
+//
+//  TKODisclosingView.m
+//  TKOProblemTemplateClassDemo
+//
+//  Created by Todd Olsen on 3/5/14.
+//  Copyright (c) 2014 Todd Olsen. All rights reserved.
+//
+
+#import "TKODisclosingView.h"
+#import "NSView+TKOKit.h"
+
+@import QuartzCore;
+
+@interface TKODisclosingView ()
+
+@property (strong, nonatomic) NSView * headerView;
+@property (strong, nonatomic) NSButton * disclosureButton;
+@property (strong, nonatomic) NSTextField * titleField;
+
+@property (strong, nonatomic) NSLayoutConstraint * heightConstraint;
+
+@end
+
+@implementation TKODisclosingView
+{
+    BOOL _disclosureIsClosed;
+}
+
+- (id)initWithTitle:(NSString *)title
+        contentView:(NSView *)contentView
+{
+    self = [super initWithFrame:NSZeroRect];
+    if (!self)
+        return nil;
+    
+    self.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self configureSubviews];
+    
+    self.title = title;
+    self.contentView = contentView;
+    
+    return self;
+}
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self)
+        return nil;
+    
+    [self configureSubviews];
+    
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self configureSubviews];
+}
+
+- (void)setTitle:(NSString *)title
+{
+    if ([_title isEqualToString:title])
+        return;
+    
+    _title = title.copy;
+    self.titleField.stringValue = _title;
+}
+
+- (void)setContentView:(NSView *)contentView
+{
+    if (_contentView == contentView)
+        return;
+    
+    [_contentView removeFromSuperview];
+    _contentView = contentView;
+    [self addSubview:_contentView];
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_contentView]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:NSDictionaryOfVariableBindings(_contentView)]];
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_headerView][_contentView]-(0@600)-|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:NSDictionaryOfVariableBindings(_headerView, _contentView)]];
+}
+
+- (void)disclosureButtonAction:(id)sender
+{
+    _disclosureIsClosed = self.disclosureButton.state;
+    [self toggleContentViewAction];
+}
+
+- (void)toggleContentViewAction
+{
+    if (!_disclosureIsClosed)
+    {
+        CGFloat distanceFromHeaderToBottom = NSMinY(self.bounds) - NSMinY(self.headerView.frame);
+        if (!self.heightConstraint)
+        {
+            self.heightConstraint = [NSLayoutConstraint constraintWithItem:self.headerView
+                                                                 attribute:NSLayoutAttributeBottom
+                                                                 relatedBy:NSLayoutRelationEqual
+                                                                    toItem:self
+                                                                 attribute:NSLayoutAttributeBottom
+                                                                multiplier:1
+                                                                  constant:distanceFromHeaderToBottom];
+        }
+        self.heightConstraint.constant = distanceFromHeaderToBottom;
+        [self addConstraint:self.heightConstraint];
+        
+        [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context) {
+            context.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+            self.heightConstraint.animator.constant = 0;
+        } completionHandler:nil];
+    }
+    else
+    {
+        [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context) {
+            context.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+            self.heightConstraint.animator.constant -= self.contentView.frame.size.height;
+        } completionHandler:^{
+            [self removeConstraint:self.heightConstraint];
+        }];
+    }
+}
+
+- (void)configureSubviews
+{
+    if (self.headerView)
+        return;
+    
+    self.headerView = [NSView viewWithClass:[NSView class]];
+    [self.headerView addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.headerView
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:47.0]];
+    
+    self.disclosureButton = [NSView viewWithClass:[NSButton class]];
+    self.disclosureButton.bezelStyle = NSDisclosureBezelStyle;
+    self.disclosureButton.buttonType = NSOnOffButton;
+    self.disclosureButton.imagePosition = NSImageOnly;
+    self.disclosureButton.target = self;
+    self.disclosureButton.action = @selector(disclosureButtonAction:);
+    self.disclosureButton.state = NSOnState;
+    [self.disclosureButton addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.disclosureButton
+                                  attribute:NSLayoutAttributeWidth
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:27]
+     ];
+    [self.disclosureButton addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.disclosureButton
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:27]
+     ];
+
+    self.titleField = [NSView viewWithClass:[NSTextField class]];
+    self.titleField.editable = NO;
+    self.titleField.selectable = NO;
+    self.titleField.bordered = NO;
+    self.titleField.backgroundColor = [NSColor controlColor];
+    self.titleField.font = [NSFont boldSystemFontOfSize:13.0];
+    self.titleField.textColor = [NSColor darkGrayColor];
+    
+    [self.headerView addSubview:self.disclosureButton];
+    [self.headerView addSubview:self.titleField];
+    
+    NSDictionary * views = NSDictionaryOfVariableBindings(_disclosureButton, _titleField);
+    [self.headerView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(8)-[_disclosureButton]-(1)-[_titleField]-(15)-|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]
+     ];
+
+    [self.headerView addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.disclosureButton
+                                  attribute:NSLayoutAttributeCenterY
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.headerView
+                                  attribute:NSLayoutAttributeCenterY
+                                 multiplier:1
+                                   constant:0]
+     ];
+
+    [self.headerView addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.disclosureButton
+                                  attribute:NSLayoutAttributeCenterY
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.titleField
+                                  attribute:NSLayoutAttributeCenterY
+                                 multiplier:1
+                                   constant:1]
+     ];
+    
+    [self addSubview:self.headerView];
+    
+    views = @{@"_headerView": _headerView};
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_headerView]"
+                                             options:0
+                                             metrics:nil
+                                               views:views]
+     ];
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_headerView]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]
+     ];
+    
+    NSBox * separator = [NSView viewWithClass:[NSBox class]];
+    [separator addConstraint:
+     [NSLayoutConstraint constraintWithItem:separator
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1
+                                   constant:1]
+     ];
+    separator.boxType = NSBoxSeparator;
+    
+    [self addSubview:separator];
+    
+    views = @{@"separator": separator};
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[separator]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]
+     ];
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[separator]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]
+     ];
+}
+
+
+
+@end
